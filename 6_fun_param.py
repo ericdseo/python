05e5313b8a89158ea718fe45faa66aa373cd5da0
Eric Seo <eric.dy.seo@gmail.com>
2025-08-30 00:25:32 +0000

    6 fun param

---
+++ 6_fun_param.py
@@ -0,0 +1,15 @@
+# def hi(name, number):
+#     for i in range(number):
+#         print("Hi!", name)
+
+# hi('Eric', 5)
+# hi('DAle', 3)
+
+def math(poopoopants):
+    for i in range(1, 10):
+        print(poopoopants, 'x', i, '=', poopoopants * i)
+        print('===============')
+    print("-------------")
+
+for i in range(2, 21):
+    math(i)
