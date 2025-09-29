cfd11021fd95630a28e281028842131a74e4a763
Eric Seo <eric.dy.seo@gmail.com>
2025-09-29 00:38:13 +0000

    8 list

--- 8_list.py
+++ 8_list.py
@@ -7,10 +7,10 @@ colors.append("yellow")
 colors.append("green")
 colors.append("pink")
 colors.append("brown poo")
-# print(colors)
+print(colors)
 
-# print(colors[3])
-# print(colors[1], colors[2])
+print(colors[3])
+print(colors[1], colors[2])
 
 colors.pop() # pop "brown poo"
 colors.pop() # pop "pink"
