9cf5ffa3e30701419c675919a1ed926995d63aab
Eric Seo <eric.dy.seo@gmail.com>
2025-10-08 18:29:14 +0000

    9 dict

---
+++ 9_dict.py
@@ -0,0 +1,18 @@
+colors = {
+    "red": "빨강",
+    "yellow":"노랑"
+}
+
+colors["green"] = "초록"
+colors["blue"] = "파랑"
+colors["rainbow"] = "무지게"
+
+# print(colors["green"])
+# print(colors["blue"])
+# print(colors["rainbow"])
+
+# red 빨강
+for english in colors:
+    korean = colors[english]
+    print(english, korean)
+    
